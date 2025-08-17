b87ecfdc79c70c4828722e8a16dc4df24c9d1fd8
narora123 <103226012+narora123@users.noreply.github.com>
2025-08-17 11:35:30 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,7 @@
 import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
-export const supabaseBrowser = () => createPagesBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export const supabaseBrowser = () =>
+  createPagesBrowserClient({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  });
+
